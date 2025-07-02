d7e86feebc798d4ef86e02d6048151c829bdea20
sowmeya013 <sowmeyapds@gmail.com>
2025-07-02 14:56:04 +0530

    class

---
+++ src/com/vetias/java/arraymark.java
@@ -0,0 +1,11 @@
+package com.vetias.java;
+
+public class arraymark {
+    public static void main(String[]args){
+        int []marks={89,99,98,97,90};
+         for(int mark:marks){
+            System.out.println(mark);
+        }
+    }
+    
+}
