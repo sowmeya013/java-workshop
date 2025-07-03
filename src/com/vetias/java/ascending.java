8c1a6e93db3c220aac8ac827024045d8190ca92c
sowmeya013 <sowmeyapds@gmail.com>
2025-07-03 10:51:14 +0530

    ascending

---
+++ src/com/vetias/java/ascending.java
@@ -0,0 +1,16 @@
+package com.vetias.java;
+
+import java.util.Arrays;
+
+public class ascending {
+    public static void main(String[]args){
+        double[] prices = {19.00,4.99,9.89,6.98,6.78};
+        
+        Arrays.sort(prices);
+
+        System.out.println("prices list in ascending order");
+        for (double price : prices) {
+            System.out.println(price);
+        }
+    } 
+}
