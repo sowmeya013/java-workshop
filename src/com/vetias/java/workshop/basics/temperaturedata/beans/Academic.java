7e5f9ec1599c8fa34151ccb06c27f10aceac32f4
sowmeya013 <sowmeyapds@gmail.com>
2025-07-03 14:59:45 +0530

    added sub class

---
+++ src/com/vetias/java/workshop/basics/temperaturedata/beans/Academic.java
@@ -0,0 +1,7 @@
+package com.vetias.java.workshop.basics.temperaturedata.beans;
+
+public class Academic  extends Buildings {
+    private boolean isLibraryAvailable;
+    private boolean isauditoriumAvailable;
+
+}
---
+++ src/com/vetias/java/workshop/basics/temperaturedata/beans/Hostel.java
@@ -0,0 +1,7 @@
+package com.vetias.java.workshop.basics.temperaturedata.beans;
+
+public class Hostel extends Buildings {
+    private boolean isMessAvailable;
+    private int numberOfRomms;
+
+}
