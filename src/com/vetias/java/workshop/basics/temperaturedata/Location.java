2151aff3167c42a6db2c3b4895a3f2c80af04446
sowmeya013 <sowmeyapds@gmail.com>
2025-07-04 14:16:16 +0530

    location java

--- src/com/vetias/java/workshop/basics/temperaturedata/Location.java
+++ src/com/vetias/java/workshop/basics/temperaturedata/Location.java
@@ -9,17 +9,62 @@ public class Location {
     private String city;
     private String address;
     private String postalcode;
-}
 
-public class Location {
-    private void setName(String aName) {
+    private void setname(String aName) {
         name= aName;
     }
     public String getName() {
         return name;
     }
-
+ public double getLatitude() {
+        return latitude;
     }
- }
 
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostalCode() {
+        return postalcode;
+    }
+
+    public String getcountry() {
+        return country;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalcode = postalCode;
+    }
+    public void setContact(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {      
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", address='" + address + '\'' +
+                ", postalCode='" + postalcode + '\'' +
+                ", contact='" + country + '\'' +
+                '}';
+    }
 }
+ 
+
