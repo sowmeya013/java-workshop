06e1ea14c0adb1d70f42a749565a34dc903cf6e3
sowmeya013 <sowmeyapds@gmail.com>
2025-07-05 23:14:05 +0530

    Update Hostel.java

--- src/com/vetias/java/workshop/basics/temperaturedata/beans/Hostel.java
+++ src/com/vetias/java/workshop/basics/temperaturedata/beans/Hostel.java
@@ -5,3 +5,30 @@ public class Hostel extends Buildings {
     private int numberOfRomms;
 
 }
+    public Hostel(int numberOfRooms, boolean isMessAvailable) {
+        this.numberOfRooms = numberOfRooms;
+        this.isMessAvailable = isMessAvailable;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public boolean isMessAvailable() {
+        return isMessAvailable;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public void setMessAvailable(boolean messAvailable) {
+        isMessAvailable = messAvailable;
+    }
+
+    public void displayHostelInfo() {
+        super.PrintDetails();
+        System.out.println("Number of Rooms: " + numberOfRooms);
+        System.out.println("Is Mess Available: " + (isMessAvailable ? "Yes" : "No"));
+    }
+}
