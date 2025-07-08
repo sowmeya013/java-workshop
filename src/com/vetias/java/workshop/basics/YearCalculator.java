0b86c718b3422a435c9d56c02bd1c8bd814ad8b7
sowmeya013 <sowmeyapds@gmail.com>
2025-07-08 11:08:03 +0530

    to get date of birth

--- src/com/vetias/java/workshop/basics/YearCalculator.java
+++ src/com/vetias/java/workshop/basics/YearCalculator.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 import java.time.LocalDate;
 public class YearCalculator {
     public static void main(String[] args) {
-        Scanner inputScanner =new Scanner(System.in);
-        System.out.println("enter the year(only year):");
-        int year=inputScanner.nextInt();
-        int currentyear=LocalDate.now().getYear();
-        int age=currentyear-year;
+        Scanner inputScanner =new Scanner(System.in);{
+        System.out.println("enter the year(YYYY-MM-DD):");
+        String dateOfBirth=inputScanner.nextLine();
+        LocalDate birthDate=LocalDate.parse(dateOfBirth);;
+        int  YearOfBirth =birthDate.getYear();
+        int currentYear=LocalDate.now().getYear();
+        int age=currentYear-YearOfBirth;
         System.out.println(age);
+    }
 
     }
 
