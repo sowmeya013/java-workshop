917d6f1a0a97358f7b348538e0bfe6edef1fd7e5
sowmeya013 <sowmeyapds@gmail.com>
2025-07-08 15:00:22 +0530

    Arthmetic Calculator

---
+++ src/com/vetias/java/workshop/basics/Calculator.java
@@ -0,0 +1,25 @@
+package com.vetias.java.workshop.basics;
+public class Calculator {
+    
+    public static void main(String[] args) {
+        int a=3,b=0;
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        try{
+            if (b==0) 
+            {
+                throw new ArithmeticException("0 cant be a divisor");
+
+                
+            }
+        }catch(ArithmeticException e){
+                System.out.println(e.getMessage());
+        }
+            
+    }
+
+}
+
+    
+  
